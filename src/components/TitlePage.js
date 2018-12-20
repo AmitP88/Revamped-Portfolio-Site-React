9406bd6c50c0d67d2a7fdab23594b6c1d0506aa4
amitp <amitpatel_7@msn.com>
2018-12-20 02:30:16 -0800

    installed npm react-animated-css and faded in title

--- src/components/TitlePage.js
+++ src/components/TitlePage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Animated } from 'react-animated-css';
 
 export default class TitlePage extends Component {
     render(){
         return (
             <div className="titlePage">
-                <h1 className="title">AMIT PATEL</h1>
+                <Animated animationIn="fadeInUp" isVisible={true}>
+                    <h1 className="title">AMIT PATEL</h1>         
+                </Animated>
                 <h3 className="subtitle typewriter">&lt;FRONT END DEVELOPER&gt;</h3>
             </div>
         );
