f65c7226d56e94408bec4b9c147c23e6c459cf1e
amitp <amitpatel_7@msn.com>
2018-12-20 01:53:38 -0800

    added title, subtitle, and typing animation to subtitle

--- src/components/TitlePage.js
+++ src/components/TitlePage.js
@@ -4,7 +4,8 @@ export default class TitlePage extends Component {
     render(){
         return (
             <div className="titlePage">
-            
+                <h1 className="title">AMIT PATEL</h1>
+                <h3 className="subtitle typewriter">&lt;FRONT END DEVELOPER&gt;</h3>
             </div>
         );
     }
