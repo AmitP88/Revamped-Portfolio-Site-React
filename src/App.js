ed5d9bd7bc0c9f1ac4b40502010818f22542b587
amitp <amitpatel_7@msn.com>
2018-12-20 00:18:25 -0800

    created TitlePage component and transferred styles from className .App to .titlaPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import TitlePage from './components/TitlePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <TitlePage />
       </div>
     );
   }
