be7b7d3315d7333fcf58936b2eba4469a24a9a89
amitp <amitpatel_7@msn.com>
2018-12-21 22:54:58 -0800

    added logos to Skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
 import { Animated } from 'react-animated-css';
 
+// Import logo images
+import HTML5_Logo from '../images/HTML_Logo.png';
+import CSS3_Logo from '../images/CSS_Logo.png';
+import JavaScript_Logo from '../images/JavaScript_Logo.png';
+import React_Logo from '../images/React_Logo.png';
+import Redux_Logo from '../images/Redux_Logo.png';
+import D3_Logo from '../images/D3_Logo.png';
+
+const logos = {
+    HTML5: HTML5_Logo,
+    CSS3: CSS3_Logo,
+    JavaScript: JavaScript_Logo,
+    React: React_Logo,
+    Redux: Redux_Logo,
+    D3: D3_Logo
+};
+
 export default class Skills extends Component {
     render(){
         return (
             <div className="Skills-container">
                 <Animated className="skills-animated" animationIn="fadeInDown" isVisible={true}>
-                    <h1 className="Skills-title">SKILLS</h1>             
+                    <h1 className="Skills-title">SKILLS</h1>
                 </Animated>
                 <div className="row">
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
+                    <div className="skill">
+                        <img src={logos.HTML5} alt={Object.keys(logos)[0]} />
+                        <h3>{Object.keys(logos)[0]}</h3>
+                    </div>
+                    <div className="skill">
+                        <img src={logos.CSS3} alt={Object.keys(logos)[1]} />
+                        <h3>{Object.keys(logos)[1]}</h3>
+                    </div>
+                    <div className="skill">
+                        <img src={logos.JavaScript} alt={Object.keys(logos)[2]} />
+                        <h3>{Object.keys(logos)[2]}</h3>
+                    </div>
+                    <div className="skill">
+                        <img src={logos.React} alt={Object.keys(logos)[3]} />
+                        <h3>{Object.keys(logos)[3]}</h3>
+                    </div>
+                    <div className="skill">
+                        <img src={logos.Redux} alt={Object.keys(logos)[4]} />
+                        <h3>{Object.keys(logos)[4]}</h3>
+                    </div>
                 </div>
-                <div className="button">
+                <div className="row">
+                    <div className="skill">
+                        <img src={logos.D3} alt={Object.keys(logos)[5]} />
+                        <h3>{Object.keys(logos)[5]}</h3>
+                    </div>
+                </div>
+                <div className="button Projects-button">
                     <a href="#" className="btn btn-white btn-animated">{this.props.buttonName}</a>                
                 </div>            
             </div>
