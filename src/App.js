53f7de182e64cc65668118ec487917577521c1a0
amitp <amitpatel_7@msn.com>
2018-12-22 07:17:31 -0800

    created new component Projects and created new css file Projects.css for new component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import '../src/css/media_queries.css';
 import TitlePage from './components/TitlePage';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
       <div className="App">
         <TitlePage buttonName="Skills" />
         <Skills buttonName="Projects" />
+        <Projects />
       </div>
     );
   }
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../css/Projects.css';
+
+export default class Projects extends Component {
+    render(){
+        return (
+            <div className="Projects-container">
+
+            </div>
+        );
+    }
+}
