6d9a73100b9c5f8b633da66f38c31b2fdb7fe0c9
amitp <amitpatel_7@msn.com>
2018-12-21 19:09:05 -0800

    created new component Skills and added title with animation

---
+++ src/components/Skills.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Animated } from 'react-animated-css';
+
+export default class Skills extends Component {
+    render(){
+        return (
+            <div className="Skills-container">
+                <Animated className="skills-animated" animationIn="fadeInDown" isVisible={true}>
+                    <h1 className="Skills-title">SKILLS</h1>                
+                </Animated>
+            </div>
+        );
+    }
+}
