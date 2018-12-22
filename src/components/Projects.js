6ec0e9f1ffd49fddae455949de1f9e1da9c2e6e0
amitp <amitpatel_7@msn.com>
2018-12-22 07:36:55 -0800

    added title to Projects component and centered it

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Animated } from 'react-animated-css';
 import '../css/Projects.css';
 
 export default class Projects extends Component {
     render(){
         return (
             <div className="Projects-container">
-
+                <Animated className="projects-animated" animationIn="fadeInDown" isVisible={true}>
+                    <h1 className="Projects-title">PROJECTS</h1>
+                </Animated>
             </div>
         );
     }
