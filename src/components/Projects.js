644aaa9057700f6ab89f078efb41c41aa0af7221
amitp <amitpatel_7@msn.com>
2018-12-22 09:58:54 -0800

    added flip card with place holder img and content to Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,19 @@ export default class Projects extends Component {
                 <Animated className="projects-animated" animationIn="fadeInDown" isVisible={true}>
                     <h1 className="Projects-title">PROJECTS</h1>
                 </Animated>
+
+                <div className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <img className="project-img" src="https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/11/zelda-ocarina-of-time-920x584.jpg" alt="Avatar" />
+                        </div>
+                        <div className="flip-card-back">
+                            <h1>John Doe</h1> 
+                            <p>Architect & Engineer</p> 
+                            <p>We love that guy</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
