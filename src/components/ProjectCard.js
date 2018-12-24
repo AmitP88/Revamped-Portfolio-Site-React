7db79b79f608a9da9dcd7ddd236133b68f6eb538
amitp <amitpatel_7@msn.com>
2018-12-24 01:29:35 -0800

    increased font size of back of card title and adjusted spacing

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -16,7 +16,7 @@ export default class ProjectCard extends Component {
               <p className="description">{this.props.description}</p>
             </div>
             <div className="flip-card-back">
-                <h3>Tech Used:</h3>
+                <h3 className="techList-title">Tech Used:</h3>
                 <ul>{this.props.techUsed}</ul>
                 <div className="Card-Demo-button">
                     <a href="#" className="btn btn-white btn-animated">Demo</a>                
