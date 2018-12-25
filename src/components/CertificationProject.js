233f796d3457f21df48c682c9603d9a12a477eb8
amitp <amitpatel_7@msn.com>
2018-12-25 07:26:54 -0800

    aligned cert project text and button positions

--- src/components/CertificationProject.js
+++ src/components/CertificationProject.js
@@ -6,12 +6,14 @@ export default class CertificationProject extends Component {
     return (
       <div className="certification-project">
         <h3 className="cert-project-name">{this.props.projectName}</h3>
-        <a href={this.props.projectDemo} target="_blank" rel="noopener noreferrer">
-          <button className="cert-project-demo">Demo</button>        
-        </a>
-        <a href={this.props.projectRepo} target="_blank" rel="noopener noreferrer">
-          <button className="cert-project-repo">Repo</button>        
-        </a>
+        <div className="cert-project-buttons">
+          <a href={this.props.projectDemo} target="_blank" rel="noopener noreferrer">
+            <button className="cert-project-demo">Demo</button>        
+          </a>
+          <a href={this.props.projectRepo} target="_blank" rel="noopener noreferrer">
+            <button className="cert-project-repo">Repo</button>        
+          </a>        
+        </div>
       </div>
     );
   }
