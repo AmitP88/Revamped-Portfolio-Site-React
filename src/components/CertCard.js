9789924ca231a0e9cec55d85a6f4246b1fcde136
amitp <amitpatel_7@msn.com>
2018-12-27 06:38:49 -0800

    changed button classes to cert-button classes in CertCard component

--- src/components/CertCard.js
+++ src/components/CertCard.js
@@ -18,39 +18,39 @@ export default class CertCard extends Component {
               <div className="cert-project">
                 <h3 className="cert-project-name">{this.props.project_1_Name}</h3>
                 <div className="buttons-container">
-                  <a href={this.props.project_1_demoLink} className="button button-white button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
-                  <a href={this.props.project_1_repoLink} className="button button-white button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
+                  <a href={this.props.project_1_demoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
+                  <a href={this.props.project_1_repoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
                 </div>
               </div>
               <div className="cert-project">
                 <h3 className="cert-project-name">{this.props.project_2_Name}</h3>
                 <div className="buttons-container">
-                  <a href={this.props.project_2_demoLink} className="button button-white button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
-                  <a href={this.props.project_2_repoLink} className="button button-white button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
+                  <a href={this.props.project_2_demoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
+                  <a href={this.props.project_2_repoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
                 </div>
               </div>
               <div className="cert-project">
                 <h3 className="cert-project-name">{this.props.project_3_Name}</h3>
                 <div className="buttons-container">
-                  <a href={this.props.project_3_demoLink} className="button button-white button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
-                  <a href={this.props.project_3_repoLink} className="button button-white button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
+                  <a href={this.props.project_3_demoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
+                  <a href={this.props.project_3_repoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
                 </div>
               </div>
               <div className="cert-project">
                 <h3 className="cert-project-name">{this.props.project_4_Name}</h3>
                 <div className="buttons-container">
-                  <a href={this.props.project_4_demoLink} className="button button-white button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
-                  <a href={this.props.project_4_repoLink} className="button button-white button-animated" target="_blank" rel="noopener noreferrer">Repo</a> 
+                  <a href={this.props.project_4_demoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
+                  <a href={this.props.project_4_repoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Repo</a> 
                 </div>
               </div>
               <div className="cert-project">
                 <h3 className="cert-project-name">{this.props.project_5_Name}</h3>
                 <div className="buttons-container">
-                  <a href={this.props.project_5_demoLink} className="button button-white button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
-                  <a href={this.props.project_5_repoLink} className="button button-white button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
+                  <a href={this.props.project_5_demoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
+                  <a href={this.props.project_5_repoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
                 </div>
               </div>
-              <a href={this.props.certLink} className="cert-link button button-lightgreen button-animated" target="_blank" rel="noopener noreferrer">Certification</a>              
+              <a href={this.props.certLink} className="cert-link cert-button cert-button-lightgreen cert-button-animated" target="_blank" rel="noopener noreferrer">Certification</a>              
             </div>
           </div>
         </div>
