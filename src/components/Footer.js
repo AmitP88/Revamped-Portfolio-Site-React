b8b8b6de1125dcd3da5e5a85854b9fb566a5e9c3
amitp <amitpatel_7@msn.com>
2018-12-28 12:36:08 -0800

    added social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import '../css/Footer.css';
 import React_Logo from '../images/React_Logo.png';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+
 
 export default class Footer extends Component {
   render(){
@@ -12,6 +16,9 @@ export default class Footer extends Component {
         <h3 className="builtWithReact">Built with <span>React <img src={React_Logo} className="footer-react-logo" alt="React_Logo"/></span></h3>
         <div className="contactIcons">
           <FontAwesomeIcon className="icon" icon={faLinkedin} />
+          <FontAwesomeIcon className="icon" icon={faTwitter} />
+          <FontAwesomeIcon className="icon" icon={faGithub} />
+          <FontAwesomeIcon className="icon" icon={faEnvelopeSquare} />
         </div>
       </footer>
     );
