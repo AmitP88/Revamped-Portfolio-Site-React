add90367230eac1ded22bd3cfb5e317bddc69e5f
amitp <amitpatel_7@msn.com>
2018-12-28 12:05:37 -0800

    added Linkedin fontawesome icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import '../css/Footer.css';
 import React_Logo from '../images/React_Logo.png';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 export default class Footer extends Component {
   render(){
     return (
       <footer>
         <h3 className="builtWithReact">Built with <span>React <img src={React_Logo} className="footer-react-logo" alt="React_Logo"/></span></h3>
+        <div className="contactIcons">
+          <FontAwesomeIcon className="icon" icon={faLinkedin} />
+        </div>
       </footer>
     );
   }
