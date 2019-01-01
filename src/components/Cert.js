377eda43d3d40519e7d2bc93675ebb653081ee39
amitp <amitpatel_7@msn.com>
2019-01-01 09:48:42 -0800

    added Modal content

--- src/components/Cert.js
+++ src/components/Cert.js
@@ -24,7 +24,43 @@ export default class Cert extends Component {
           <button onClick={this.onOpenModal} className="cert-link cert-button cert-button-lightgreen cert-button-animated" target="_blank" rel="noopener noreferrer">PROJECTS</button>       
         </div>
         <Modal open={open} onClose={this.onCloseModal} center>
-          <h1>Projects</h1>
+          <h2>Projects</h2>
+          <div className="cert-project">
+            <h3 className="cert-project-name">{this.props.project_1_Name}</h3>
+            <div className="buttons-container">
+              <a href={this.props.project_1_demoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
+              <a href={this.props.project_1_repoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
+            </div>
+          </div>
+          <div className="cert-project">
+            <h3 className="cert-project-name">{this.props.project_2_Name}</h3>
+            <div className="buttons-container">
+              <a href={this.props.project_2_demoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
+              <a href={this.props.project_2_repoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
+            </div>
+          </div>
+          <div className="cert-project">
+            <h3 className="cert-project-name">{this.props.project_3_Name}</h3>
+            <div className="buttons-container">
+              <a href={this.props.project_3_demoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
+              <a href={this.props.project_3_repoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
+            </div>
+          </div>
+          <div className="cert-project">
+            <h3 className="cert-project-name">{this.props.project_4_Name}</h3>
+            <div className="buttons-container">
+              <a href={this.props.project_4_demoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
+              <a href={this.props.project_4_repoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
+            </div>
+          </div>
+          <div className="cert-project">
+            <h3 className="cert-project-name">{this.props.project_5_Name}</h3>
+            <div className="buttons-container">
+              <a href={this.props.project_5_demoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Demo</a>
+              <a href={this.props.project_5_repoLink} className="cert-button cert-button-white cert-button-animated" target="_blank" rel="noopener noreferrer">Repo</a>
+            </div>
+          </div>
+          <a href={this.props.certLink} className="cert-link cert-button cert-button-lightgreen cert-button-animated" target="_blank" rel="noopener noreferrer">Certification</a> 
         </Modal>
       </div>
     );
