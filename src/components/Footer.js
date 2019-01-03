93912f18eb0cd569d60542b5f613ee111bfbaf3d
amitp <amitpatel_7@msn.com>
2019-01-02 17:12:17 -0800

    added links to social media icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,11 +17,21 @@ export default class Footer extends Component {
         <div className="ending-statement">
           <h4 className="builtWithReact">Built with <span>React <img src={React_Logo} className="footer-react-logo" alt="React_Logo"/></span></h4>
           <div className="contactIcons">
-            <FontAwesomeIcon className="icon" icon={faLinkedin} size="2x" />
-            <FontAwesomeIcon className="icon" icon={faTwitter} size="2x" />
-            <FontAwesomeIcon className="icon" icon={faGithub} size="2x" />
-            <FontAwesomeIcon className="icon" icon={faEnvelope} size="2x" />
-            <FontAwesomeIcon className="icon" icon={faWhatsapp} size="2x" />
+            <a href="https://www.linkedin.com/in/amitp88/" className="icon" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon  icon={faLinkedin} size="2x" />          
+            </a>
+            <a href="https://twitter.com/AmitPateldev" className="icon" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />          
+            </a>
+            <a href="https://github.com/AmitP88" className="icon" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} size="2x" />          
+            </a>
+            <a href="mailto: amitpatel_7@msn.com" className="icon" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faEnvelope} size="2x" />          
+            </a>
+            <a href="https://api.whatsapp.com/send?phone=15593477496" className="icon" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faWhatsapp} size="2x" />          
+            </a>
           </div>                 
           <p className="copyright">&copy; Copyright</p>        
         </div>
