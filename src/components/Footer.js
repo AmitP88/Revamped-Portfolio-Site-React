58e84f4abc35ed72bdbea04f0cdec20c926b0058
amitp <amitpatel_7@msn.com>
2019-01-13 04:43:30 -0800

    changed Envelope icon to a different Envelope icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 
