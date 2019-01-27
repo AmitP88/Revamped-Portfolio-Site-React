7d88f6cc8945b22774e0979c42dda8c5c5485f00
amitp <amitpatel_7@msn.com>
2019-01-26 20:03:57 -0800

    changed hover color of ToolBar elements and decreased size of profilePhoto on 1024+ px viewports

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/ToolBar.css';
 
 import DrawerToggleButton from '../components/DrawerToggleButton';
 
-const ToolBar = (props) => {
-  return (
-    <header className="ToolBar">
-      <nav className="ToolBar-navigation">
-        <div className="toolbar-toggle-button">
-          <DrawerToggleButton click={props.drawerClickHandler} />
-        </div>
-        <div className="ToolBar-logo"><a href="/">&lt;A&gt;</a></div>
-        <div className="spacer" />
-        <div className="ToolBar-navigation-items">
-          <ul>
-            <li><a href='/'>Skills</a></li>
-            <li><a href='/'>Projects</a></li>
-            <li><a href='/'>Certifications</a></li>
-            <li><a href='/'>About Me</a></li>
-          </ul>        
-        </div>
-      </nav>      
-    </header>
-  );
+class ToolBar extends Component {
+  render() {
+    return (
+      <header className="ToolBar">
+        <nav className="ToolBar-navigation">
+          <div className="toolbar-toggle-button">
+            <DrawerToggleButton click={this.props.drawerClickHandler} />
+          </div>
+          <div className="ToolBar-logo"><a href="/">&lt;A&gt;</a></div>
+          <div className="spacer" />
+          <div className="ToolBar-navigation-items">
+            <ul>
+              <li><a href='/'>Skills</a></li>
+              <li><a href='/'>Projects</a></li>
+              <li><a href='/'>Certifications</a></li>
+              <li><a href='/'>About Me</a></li>
+            </ul>        
+          </div>
+        </nav>      
+      </header>
+    );    
+  }
+
 }
 
 export default ToolBar;
